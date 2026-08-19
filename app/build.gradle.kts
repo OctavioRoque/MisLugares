8458cc8ca0842ae06e1ded278f49c915afe424d2
Octavio Roque <or94502@gmail.com>
2026-08-19 07:43:59 -0600

    Fui un pendejo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,11 +29,16 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.legacy.support.v4)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
