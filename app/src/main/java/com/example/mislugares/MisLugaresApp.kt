c1f0fc912269cd1fe04c0d6f87b4813a22306011
Diego <dadrianlogo@gmail.com>
2026-09-21 15:08:16 -0600

    fix error abrir app

--- app/src/main/java/com/example/mislugares/MisLugaresApp.kt
+++ app/src/main/java/com/example/mislugares/MisLugaresApp.kt
@@ -2,6 +2,7 @@ package com.example.mislugares
 
 import android.app.Application
 import org.osmdroid.config.Configuration
+import android.preference.PreferenceManager
 
 /**
  * Clase Application para inicializaciones globales.
@@ -12,6 +13,7 @@ class MisLugaresApp : Application() {
         
         // Configuración de osmdroid según la política de OpenStreetMap
         // El User-Agent debe ser único para evitar el bloqueo 403
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
         Configuration.getInstance().userAgentValue = "MisLugaresApp/1.0 (com.example.mislugares)"
     }
 }
